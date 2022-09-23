0ad80e40ba82ab6bb3b7caf588185e3598f8042b
dudghks117 <42609006+dudghks117@users.noreply.github.com>
2022-09-23 13:40:56 +0900

    message

---
+++ src/Layout.js
@@ -0,0 +1,24 @@
+import { Outlet, useNavigate } from 'react-router-dom';
+
+const Layout = () => {
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1); // 이전페이지로 이동
+    };
+    const goArticles = () => {
+        navigate('/articles'); // articles 페이지로 이동
+    }
+    return (
+        <div>
+            <header style={{ background: 'lightgray', padding:16, fontSize:24}}>
+                <button onClick={goBack}>뒤로가기</button>
+                <button onClick={goArticles}>게시글 목록</button>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+};
+
+export default Layout;
